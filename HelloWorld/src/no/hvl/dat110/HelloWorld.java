1ebe715db5e5b39ff54f589fa73fb1d15f412c2a
sivertsaeter <sivertsaeter@192.168.0.82>
2023-01-22 21:57:03 +0100

    nytt HelloWorld prosjekt

---
+++ HelloWorld/src/no/hvl/dat110/HelloWorld.java
@@ -0,0 +1,17 @@
+package no.hvl.dat110;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("-------------");
+		
+		for(int i = 0; i < 10; i++) {
+			
+		
+		System.out.println("*");
+		}
+	}
+}
